38e4656d44e04a14e9c010a8c8ef7f2b07adf2a2
Grayson Owen <graysonowen@Graysons-MacBook-Pro.local>
2020-06-23 11:12:58 -0500

    subjects game

---
+++ FavSubjectz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FavSubjectz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Science":
+                    Console.WriteLine("Science Stinks!");
+                    break;
+
+                case "Math":
+                    Console.WriteLine("Math is the worst!");
+                    break;
+
+                case "English":
+                    Console.WriteLine("English is for dummies!!");
+                    break;
+                case "History":
+                    Console.WriteLine("Eh, History is old!!");
+                    break;
+                case "Coding":
+                    Console.WriteLine("Ayyyyyeeee Coding Rules!");
+                    break;
+                default:
+                    Console.WriteLine("Thats not a subject you dummy!!");
+                    break;
+
+            }
+        }
+    }
+}
